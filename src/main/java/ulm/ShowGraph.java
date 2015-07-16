749e9421a6fb289fce99170684568d9fa8395a1f
gleich <gleich@vsp.tu-berlin.de>
2015-07-16 17:29:57 +0200

    ShowGraph graph path adjusted for new otp version 0.18

--- src/main/java/ulm/ShowGraph.java
+++ src/main/java/ulm/ShowGraph.java
@@ -7,7 +7,7 @@ public class ShowGraph {
 
     // Das funktioniert zwar, das Programm scheint aber nicht gepflegt zu sein. Ich kann damit gar nichts anfangen. mz
     public static void main(String[] args) {
-        GraphVisualizer graphVisualizer = new GraphVisualizer(OTPTripRouterFactory.createGraphService(Consts.BASEDIR + "Graph.obj").getRouter());
+        GraphVisualizer graphVisualizer = new GraphVisualizer(OTPTripRouterFactory.createGraphService(Consts.BASEDIR).getRouter());
         graphVisualizer.setVisible(true);
     }
 
